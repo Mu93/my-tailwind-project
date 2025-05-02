6bb3feca08610390f1ec336aab2137b852651cd9
Mu93 <muhammed.abdalhakeem@gmail.com>
2025-05-03 00:29:25 +0300

    add SellAnywhere comp

---
+++ src/components/SellAnywhere.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const SellAnywhere = () => {
+  return (
+    <>
+      <div>SellAnywhere</div>
+      <div>SellAnywhere</div>
+      <div>SellAnywhere</div>
+      <div>SellAnywhere</div>
+      <div>SellAnywhere</div>
+      <div>SellAnywhere</div>
+      <div>SellAnywhere</div>
+      <div>SellAnywhere</div>
+      <div>SellAnywhere</div>
+      <div>SellAnywhere</div>
+      <div>SellAnywhere</div>
+    </>
+  );
+};
